5a18ccb3e1bb9902da6291cb63427233890af93a
hkrwpf <90589842+hkrwpf@users.noreply.github.com>
2021-09-15 19:07:06 +0800

    Add files via upload

--- day2.py
+++ day2.py
@@ -1,17 +1,16 @@
 import random
-i = 1
-money = 500
 ran = random.randint(0, 80)
-while i <= 3 or money < 100:
+i = 0
+money = 500
+while money >= 100 or i < 3:
     num = int(input("请输入一个数字"))
-    money = money-100
     i += 1
+    money = money - 100
     if num == ran:
         money += 10
         print("OK")
-        break
     elif num > ran:
         print("猜大了")
     else:
         print("猜小了")
-    print(money, "￥")
+        break
