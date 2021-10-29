cffd5288121a38a7277537a0b5cb0733f07708a3
hkrwpf <90589842+hkrwpf@users.noreply.github.com>
2021-10-29 11:54:06 +0800

    Add files via upload

---
+++ test/initpage.py
@@ -0,0 +1,27 @@
+import time
+
+
+
+class InitPage:
+
+    login_success_data = [
+        {"username": "jason", "pwd": "1234567", "expect": "Student Login"},
+        {"username": "admin", "pwd": "root", "expect": "Student Login"}
+        # {"username": "wpf", "pwd": "123456", "expect": "Student Login"}
+    ]
+
+    login_error_data = [
+        {"username": "jason1", "pwd": "1234567", "expect": "账户名错误或密码错误!别瞎弄!"},
+        {"username": "admin", "pwd": "rooot", "expect": "账户名错误或密码错误!别瞎弄!"}
+    ]
+
+
+
+
+
+
+# now = time.strftime("%Y-%m-%d-%H_%M_%S",time.localtime(time.time()))
+# print(now)
+
+
+
